f8066635482440c1f2c50ccfd08078ddfa69f83b
pidderliao <guanwei0702@gmail.com>
2022-06-08 20:53:06 +0800

    add heroku-postman

--- connections/index.js
+++ connections/index.js
@@ -7,7 +7,7 @@ const DB = process.env.DATABASE.replace(
     process.env.DATABASE_PASSWORD
 );
 // console.log(process.env.PORT);
-// mongoose.connect("mongodb://localhost:27017/test")
-mongoose.connect(DB)
+mongoose.connect("mongodb://localhost:27017/test")
+// mongoose.connect(DB)
 .then(() => console.log('資料庫連接成功'))
 .catch(error=> console.log(error));
